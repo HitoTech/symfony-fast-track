24b146e1e5e5344c0a25f7313d6e4d122b48bede
Mickael Bellenger <mickael.bellenger@gmail.com>
2025-09-29 10:01:27 +0200

    Add HTTP cache purge route and update comment review route in AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Message\CommentMessage;
-use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpCache\StoreInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Workflow\WorkflowInterface;
 use Twig\Environment;
 
+#[Route('/admin')]
 final class AdminController extends AbstractController
 {
     public function __construct(
@@ -23,7 +25,7 @@ final class AdminController extends AbstractController
     ) {
     }
 
-    #[Route('/admin/comment/review/{id}', name: 'review_comment')]
+    #[Route('/comment/review/{id}', name: 'review_comment')]
     public function reviewComment(Request $request, Comment $comment, WorkflowInterface $commentStateMachine): Response
     {
         $accepted = !$request->query->get('reject');
@@ -47,5 +49,17 @@ final class AdminController extends AbstractController
             'transition' => $transition,
             'comment' => $comment,
         ]));
+    }
+
+    #[Route('/http-cache/{uri<.*>}', methods: ['PURGE'])]
+    public function purgeHttpCache(KernelInterface $kernel, Request $request, string $uri, StoreInterface $store): Response
+    {
+        if ('prod' === $kernel->getEnvironment()) {
+            return new Response('KO', 400);
+        }
+
+        $store->purge($request->getSchemeAndHttpHost().'/'.$uri);
+
+        return new Response('Done');
     }
 }
